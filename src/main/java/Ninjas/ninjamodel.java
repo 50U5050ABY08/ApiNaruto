1a87e9307e836702892082dcabef825dfdc60da8
Breno Rocha <brenorbaraujo2@gmail.com>
2026-06-08 20:20:41 -0300

    Modelagem do DB, ninjamodel e Missoesmodel concluidos!   anotations feitas para relacionamento de tabelas

--- src/main/java/Ninjas/ninjamodel.java
+++ src/main/java/Ninjas/ninjamodel.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package dev.breno.ApiNaruto.model;
+package Ninjas;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
