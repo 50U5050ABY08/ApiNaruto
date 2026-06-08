7472c7b3404854983e0cbce06c2490448073308e
Breno Rocha <brenorbaraujo2@gmail.com>
2026-06-08 18:02:59 -0300

    Cria uma entidade no banco de dados a partir do ninjacontroller

---
+++ src/main/java/dev/breno/ApiNaruto/controller/ninjacontroller.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dev.breno.ApiNaruto.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author brenorocha
+ */
+@RestController
+@RequestMapping
+public class ninjacontroller {
+    
+    @GetMapping("/funfando")
+    public String funfando() {
+   
+        return "Só pra saber se essa merda de rota ta funcionando, nesta merda";
+    }
+    
+}
---
+++ src/main/java/dev/breno/ApiNaruto/model/ninjamodel.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dev.breno.ApiNaruto.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+/**
+ *
+ * @author brenorocha
+ */
+@Entity
+@Table(name ="tb_cadastro")
+
+public class ninjamodel {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)        
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+
+    public ninjamodel() {
+    }
+
+    public ninjamodel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    
+    
+    
+    
+}
