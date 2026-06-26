00987a16e0e572848df8cf3b08d6f1e66e15a736
Breno Rocha <brenorbaraujo2@gmail.com>
2026-06-26 19:40:38 -0300

    feat(frontend): initialize React and TypeScript project with Vite | iniciar projeto React e TypeScript com Vite

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+  const titulo: string = 'API Naruto'
+
+  return (
+    <main>
+      <h1>{titulo}</h1>
+
+      <p>
+        Front-end desenvolvido com React e TypeScript.
+      </p>
+
+      <button>Listar ninjas</button>
+    </main>
+  )
+}
+
+export default App
